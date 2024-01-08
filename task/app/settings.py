1189f9248cbb401940cc4a41ef80038e8e2656ce
Александр <sasho1811@mail.ru>
2024-01-08 20:50:10 +0300

    правки

--- task/app/settings.py
+++ task/app/settings.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     amqp_url: str = "amqp://guest:guest@maprac6-rabbitmq-1:5672"
-    postgres_url: str = "postgresql://secUREusER:StrongEnoughPassword)@51.250.26.59:5432/tasks"
+    postgres_url: str = "postgresql://secUREusER:StrongEnoughPassword)@51.250.26.59:5432/querys"
     port: int = 80
 
     model_config = SettingsConfigDict(env_file='.env')
